478a5ce98189159bebbddf8f259744428e42411f
Priyank Tejani <priyanktejani7@gmail.com>
2023-07-08 22:56:49 +0100

    Add resources

---
+++ lib/src/resources/app_sizes.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+/// Sizes to be used in the app (paddings, gaps, rounded corners etc.)
+class AppSizes {
+  static const s4 = 4.0;
+  static const s8 = 8.0;
+  static const s6 = 6.0;
+  static const s12 = 12.0;
+  static const s16 = 16.0;
+  static const s18 = 16.0;
+  static const s20 = 20.0;
+  static const s22 = 22.0;
+  static const s24 = 24.0;
+  static const s32 = 32.0;
+  static const s48 = 48.0;
+  static const s50 = 50.0;
+  static const s64 = 64.0;
+}
+
+/// Gap widths
+const gapW4 = SizedBox(width: AppSizes.s4);
+const gapW8 = SizedBox(width: AppSizes.s8);
+const gapW12 = SizedBox(width: AppSizes.s12);
+const gapW16 = SizedBox(width: AppSizes.s16);
+const gapW20 = SizedBox(width: AppSizes.s20);
+const gapW24 = SizedBox(width: AppSizes.s24);
+const gapW32 = SizedBox(width: AppSizes.s32);
+const gapW48 = SizedBox(width: AppSizes.s48);
+const gapW64 = SizedBox(width: AppSizes.s64);
+
+/// Gap heights
+const gapH4 = SizedBox(height: AppSizes.s4);
+const gapH8 = SizedBox(height: AppSizes.s8);
+const gapH12 = SizedBox(height: AppSizes.s12);
+const gapH16 = SizedBox(height: AppSizes.s16);
+const gapH20 = SizedBox(height: AppSizes.s20);
+const gapH24 = SizedBox(height: AppSizes.s24);
+const gapH32 = SizedBox(height: AppSizes.s32);
+const gapH48 = SizedBox(height: AppSizes.s48);
+const gapH64 = SizedBox(height: AppSizes.s64);
---
+++ lib/src/resources/app_texts.dart
@@ -0,0 +1,7 @@
+class AppTexts {
+  static const appName = 'airSense';
+  static const no2 = 'NO2';
+  static const pm25 = 'PM25';
+  static const ino2 = 'INO2';
+  static const ipm25 = 'IPM25';
+}
