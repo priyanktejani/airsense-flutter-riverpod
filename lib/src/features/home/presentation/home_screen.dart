e360160b25fa15dd2848400575fcef8404c2f66f
Priyank Tejani <priyanktejani7@gmail.com>
2023-07-09 16:00:23 +0100

    Add graph screen navigation

--- lib/src/features/home/presentation/home_screen.dart
+++ lib/src/features/home/presentation/home_screen.dart
@@ -4,13 +4,16 @@ import 'package:airsense/src/resources/app_colors.dart';
 import 'package:airsense/src/resources/app_sizes.dart';
 import 'package:airsense/src/features/home/presentation/widgets/outlined_icon_button.dart';
 import 'package:airsense/src/resources/app_texts.dart';
+import 'package:airsense/src/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+
     // generate south kensington graph (if isNO2 = false than PM25)
     Future<void> generate(bool isNO2) async {
       final graphNodeParams = GraphNodeParams(
@@ -19,6 +22,13 @@ class HomeScreen extends ConsumerWidget {
         startTime: DateTime(2023, 1, 1),
         endTime: DateTime(2023, 6, 30),
         averaging: 'Hourly',
+      );
+
+     final String species = isNO2 ? AppTexts.no2 : AppTexts.pm25;
+      context.pushNamed(
+        AppRoute.graph.name,
+        pathParameters: {'species': species},
+        extra: graphNodeParams,
       );
     }
 
@@ -66,7 +76,10 @@ class HomeScreen extends ConsumerWidget {
             ),
             gapH16,
             OutlinedIconButton(
-              onPressed: () {},
+              onPressed: () {
+                // TODO Not implemented.
+                context.goNamed(AppRoute.customeGraphInput.name);
+              },
               text: '${AppTexts.custome} ${AppTexts.customeEmoji}',
             ),
           ],
