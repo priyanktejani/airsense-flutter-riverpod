737d2caa1adbb30be1f6a0566122e7bb01a9f361
Priyank Tejani <priyanktejani7@gmail.com>
2023-07-09 15:52:36 +0100

    Add page not found screen

---
+++ lib/src/router/not_found_screen.dart
@@ -0,0 +1,18 @@
+import 'package:airsense/src/common_widgets/empty_placeholder_widget.dart';
+import 'package:airsense/src/resources/app_texts.dart';
+import 'package:flutter/material.dart';
+
+/// Simple not found screen used for 404 errors (page not found on web)
+class NotFoundScreen extends StatelessWidget {
+  const NotFoundScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const EmptyPlaceholderWidget(
+        message: AppTexts.pageNotFound,
+      ),
+    );
+  }
+}
